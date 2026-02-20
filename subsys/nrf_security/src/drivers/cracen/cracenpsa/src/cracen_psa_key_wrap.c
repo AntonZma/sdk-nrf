85e7754276afd819951c8771d16939aecb3e34ba
Anton Zyma <anton.zyma@nordicsemi.no>
2026-02-20 12:51:12 +0200

    nrf_security: CRACEN: Add support for unwrapped key storage for KMU
    Added the ability to store keys to the KMU just after being unwrapped
    or exporting keys from the KMU before being wrapped
    by AES-KW or AES-KWP algorithm.
    
    The functionality is added to the Cracen driver wrapper.
    
    Signed-off-by: Anton Zyma <anton.zyma@nordicsemi.no>

--- subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_key_wrap.c
+++ subsys/nrf_security/src/drivers/cracen/cracenpsa/src/cracen_psa_key_wrap.c
@@ -9,7 +9,9 @@
 
 #include <psa/crypto.h>
 #include <psa/crypto_extra.h>
+#include <psa/crypto_values.h>
 
+#include <cracen/cracen_kmu.h>
 #include <internal/key_wrap/cracen_key_wrap_kw.h>
 #include <internal/key_wrap/cracen_key_wrap_kwp.h>
 
@@ -21,6 +23,26 @@ psa_status_t cracen_wrap_key(const psa_key_attributes_t *wrapping_key_attributes
 			     uint8_t *data, size_t data_size, size_t *data_length)
 {
 	psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
+
+#if defined(PSA_NEED_CRACEN_KMU_DRIVER)
+	uint8_t exported_key[CRACEN_KMU_MAX_KEY_SIZE];
+	size_t exported_key_length;
+
+	psa_key_location_t key_location =
+		PSA_KEY_LIFETIME_GET_LOCATION(psa_get_key_lifetime(key_attributes));
+
+	if (key_location == PSA_KEY_LOCATION_CRACEN_KMU) {
+		status = cracen_export_key(key_attributes, key_data, key_size,
+					   exported_key, sizeof(exported_key),
+					   &exported_key_length);
+
+		if (status != PSA_SUCCESS) {
+			goto exit;
+		}
+		key_data = exported_key;
+		key_size = exported_key_length;
+	}
+#endif /* PSA_NEED_CRACEN_KMU_DRIVER */
 
 	if (IS_ENABLED(PSA_NEED_CRACEN_AES_KW) && alg == PSA_ALG_KW) {
 		status = cracen_key_wrap_kw_wrap(wrapping_key_attributes, wrapping_key_data,
@@ -35,6 +57,11 @@ psa_status_t cracen_wrap_key(const psa_key_attributes_t *wrapping_key_attributes
 		status = PSA_ERROR_NOT_SUPPORTED;
 	}
 
+exit:
+#if defined(PSA_NEED_CRACEN_KMU_DRIVER)
+	safe_memzero(exported_key, sizeof(exported_key));
+#endif /* PSA_NEED_CRACEN_KMU_DRIVER */
+
 	return status;
 }
 
@@ -46,6 +73,24 @@ psa_status_t cracen_unwrap_key(const psa_key_attributes_t *attributes,
 			       uint8_t *key, size_t key_size, size_t *key_length)
 {
 	psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
+
+#if defined(PSA_NEED_CRACEN_KMU_DRIVER)
+	uint8_t *key_represent_buf = key;
+	size_t key_represent_size = key_size;
+	size_t *key_represent_length = key_length;
+	size_t key_bits;
+	uint8_t unwrapped_key[CRACEN_KMU_MAX_KEY_SIZE];
+	size_t unwrapped_key_length;
+
+	psa_key_location_t key_location =
+		PSA_KEY_LIFETIME_GET_LOCATION(psa_get_key_lifetime(attributes));
+
+	if (key_location == PSA_KEY_LOCATION_CRACEN_KMU) {
+		key = unwrapped_key;
+		key_size = sizeof(unwrapped_key);
+		key_length = &unwrapped_key_length;
+	}
+#endif /* PSA_NEED_CRACEN_KMU_DRIVER */
 
 	if (IS_ENABLED(PSA_NEED_CRACEN_AES_KW) && alg == PSA_ALG_KW) {
 		status = cracen_key_wrap_kw_unwrap(wrapping_key_attributes, wrapping_key_data,
@@ -59,6 +104,22 @@ psa_status_t cracen_unwrap_key(const psa_key_attributes_t *attributes,
 	} else {
 		status = PSA_ERROR_NOT_SUPPORTED;
 	}
+
+#if defined(PSA_NEED_CRACEN_KMU_DRIVER)
+	if (key_location == PSA_KEY_LOCATION_CRACEN_KMU) {
+		status = cracen_import_key(attributes, key, *key_length, key_represent_buf,
+					   key_represent_size, key_represent_length,
+					   &key_bits);
+		if (status == PSA_SUCCESS) {
+			/** Keeping key length equal to the actual key size
+			 *  since Oberon expects this number to be equal to
+			 *  the one stored in key attributes.
+			 */
+			*key_represent_length = PSA_BITS_TO_BYTES(key_bits);
+		}
+	}
+	safe_memzero(unwrapped_key, sizeof(unwrapped_key));
+#endif /* PSA_NEED_CRACEN_KMU_DRIVER */
 
 	return status;
 }
